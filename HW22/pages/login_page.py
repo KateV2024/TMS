7c8b05701d9a630ee8a1e72118b143021c78d8d6
Kate Vauchok <kate.makar2024@gmail.com>
2025-02-07 13:46:37 +0100

    Homework22

---
+++ HW22/pages/login_page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from HW22.pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    USERNAME_INPUT = (By.ID, "user-name")
+    PASSWORD_INPUT = (By.ID, "password")
+    LOGIN_BUTTON = (By.ID, "login-button")
+    ERROR_MESSAGE = (By.CLASS_NAME, "error-message-container")
+
+    def get_login_page(self):
+        self.open_url("https://www.saucedemo.com/")
+
+    def enter_username(self, username):
+        self.enter_text(self.USERNAME_INPUT, username)
+
+    def enter_password(self, password):
+        self.enter_text(self.PASSWORD_INPUT, password)
+
+    def click_login(self):
+        self.click_element(self.LOGIN_BUTTON)
+
+    def get_error_message(self):
+        return self.find_element(self.ERROR_MESSAGE).text
+
+    def valid_login(self):
+        self.get_login_page()
+        self.enter_username(name)
+        self.enter_password(password)
+        self.click_login()
